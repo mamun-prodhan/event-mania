66649b4c6ffb0af7de786eccb612dec13539d4f0
mamun-prodhan <mamunpr9@gmail.com>
2023-10-10 15:53:58 +0600

    banner completed

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,7 +1,26 @@
+import banner2 from "../../assets/banner2.png";
 const Banner = () => {
   return (
-    <div>
-      <img src="" alt="" />
+    <div className="relative mb-20 mt-10">
+      <img className="rounded-t-xl" src={banner2} alt="Banner" />
+      <div className="absolute inset-0 bg-black opacity-50 rounded-t-xl"></div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <h2 className=" text-white text-4xl text-center">The Biggest</h2>
+        <h2 className="my-4 text-white text-6xl font-bold text-center">
+          Event Planner
+        </h2>
+        <p className="text-white text-xl  text-center tracking-[4px]">
+          Every Event Should be perfect
+        </p>
+        {/* <div className="text-center mt-10 font-bold">
+          <button className="px-6 py-2 me-4 rounded-3xl text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            About Us
+          </button>
+          <button className="px-6 py-2 rounded-3xl text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            Get Started
+          </button>
+        </div> */}
+      </div>
     </div>
   );
 };
